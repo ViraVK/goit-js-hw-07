acd414f03f35b45ac6c771d2796e7bb74ecf94d6
Vira Kovalchuk <kovalchuk.vira@gmail.com>
2025-03-19 19:51:58 +0200

    hw7

--- js/task-4.js
+++ js/task-4.js
@@ -3,15 +3,18 @@ formEl.addEventListener('submit', event => {
   event.preventDefault();
 
   const elements = event.target.elements;
+
+  if (
+    elements.email.value.trim() === '' ||
+    elements.password.value.trim() === ''
+  ) {
+    alert('All form fields must be filled in');
+    return;
+  }
   const formData = {
     email: elements.email.value.trim(),
     password: elements.password.value.trim(),
   };
-  if (elements.email.name === '' || elements.password.name === '') {
-    alert('All form fields must be filled in');
-    return;
-  }
-
   console.log(formData);
   formEl.reset();
 });
